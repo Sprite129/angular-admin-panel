dd41f29d9dc107975adafed85bdb4a6fd6c8b432
Sprite129 <patrondimon1290@gmail.com>
2026-01-30 00:48:28 +0200

    user card design +angular schema fix

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { UserCard } from "./components/user-card/user-card.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, UserCard],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
